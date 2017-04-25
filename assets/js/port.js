4b5f5714a5f9b813da26f43b1086977169a9555a
mat335 <mat335@gmail.com>
2017-04-25 07:23:11 -0400

    Update port.js

--- assets/js/port.js
+++ assets/js/port.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
 var apiKey  = 'hv4e3aXYnB4FC5J1zHCJcK9bofZXuEXN';
 var userID  = 'mat335';
 
+    //Header image
 (function() {
     var behanceUserAPI = 'http://www.behance.net/v2/users/'+ userID +'?callback=?&api_key='+ apiKey;
     function setUserTemplate() {
@@ -25,5 +26,31 @@ var userID  = 'mat335';
     };
 })();
 
+    
+ //Porfolio Images   
+ (function() {
+    var perPage = 12;
+    var behanceProjectAPI = 'http://www.behance.net/v2/users/'+ userID +'/projects?callback=?&api_key=' + apiKey + '&per_page=' + perPage;
+ 
+    function setPortfolioTemplate() {
+        var projectData = JSON.parse(sessionStorage.getItem('behanceProject')),
+            getTemplate = $('#portfolio-template').html(),
+            template    = Handlebars.compile(getTemplate),
+            result      = template(projectData);
+        $('#portfolio').html(result);
+    };
+ 
+    if(sessionStorage.getItem('behanceProject')) {
+        setPortfolioTemplate();
+    } else {
+        $.getJSON(behanceProjectAPI, function(project) {
+            var data = JSON.stringify(project);
+            sessionStorage.setItem('behanceProject', data);
+            setPortfolioTemplate();
+        });
+    };
+})();   
+    
+    
 
 });
